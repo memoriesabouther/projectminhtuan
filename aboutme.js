6adf0b3db67dfb068d06c4bff7aef881cd81ba33
TUAN NGUYEN <116785039+memoriesabouther@users.noreply.github.com>
2023-05-19 18:45:23 +0700

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('.button');
+    button.addEventListener('click', function() {
+      alert('Cám ơn đã liên hệ với tôi!');
+    });
+    window.onload = function() {
+      alert("Chào mừng bạn đến với trang web của Nguyễn Minh Tuấn(aka Tuấn k3 boiz lụy tình! mssv2006210303");
+    }
